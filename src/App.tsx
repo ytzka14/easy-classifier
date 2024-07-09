ab7a0f576d07c5fed61aab1ab4b414d25a8266e2
Minhyeong Kim <ytzka14@snu.ac.kr>
2024-07-09 16:32:44 +0900

    base function done

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
 import { useState } from 'react';
 import { Classifyee, Classification, ClassifyeeComponent, ClassificationComponent } from './Classification';
+import items from "./items.json";
 
 const App = () => {
   const [ clStarted, setClStarted ] = useState(false);
   const [ clFinished, setClFinished ] = useState(false);
-  const [ toClassify, setToClassify ] = useState<Classifyee[]>([]);
+  const [ toClassify, setToClassify ] = useState<Classifyee[]>(items);
   const [ classified, setClassified ] = useState<Classification>({classes: []});
   const [ inputValue, setInputValue ] = useState("");
 
   return (
     <div>
+      <div>
+        <button onClick={() => {
+          setClStarted(false);
+          setClFinished(false);
+          setToClassify(items);
+          setClassified({classes: []});
+        }}>Reset</button>
+      </div>
+      {(!clStarted && toClassify.length === 0 &&
+        <div>
+          No items to classify...
+        </div>
+      )}
       {(!clStarted && toClassify.length > 0 &&
         <button onClick={() => {setClStarted(true);}}>
           Start Classifying!
@@ -34,16 +48,17 @@ const App = () => {
                 </div>
               );
             })}
-            <div onClick={() => {
-              const newClassified = classified;
-              newClassified.classes.push({name: inputValue, members: [toClassify[0]]});
-              setClassified(newClassified);
-              if (toClassify.length === 1) {
-                setClFinished(true);
-              } 
-              setToClassify(toClassify.slice(1));
-            }}>
+            <div>
               <input type="text" value={inputValue} onChange={(e) => {setInputValue(e.target.value);}} />
+              <button onClick={() => {
+                const newClassified = classified;
+                newClassified.classes.push({name: inputValue, members: [toClassify[0]]});
+                setClassified(newClassified);
+                if (toClassify.length === 1) {
+                  setClFinished(true);
+                } 
+                setToClassify(toClassify.slice(1));
+              }}>Submit New Class</button>
             </div>
           </div>
         </div>
